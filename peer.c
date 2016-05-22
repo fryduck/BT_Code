bb1509f5852f3303afd38eefd21fb5e42303b46c
fryduck <305775393@qq.com>
2016-05-22 13:02:49 +0900

    BT: Add two funcs into the file "peer.c"
    Details:
        Add func "free_peer_node"
        Add func "release_memory_in_peer"

--- peer.c
+++ peer.c
@@ -15,69 +15,69 @@
 extern Bitmap *bitmap;
 
 // 指向当前与之进行通信的peer链表
-Peer *peer_head=NULL;
+Peer *peer_head = NULL;
 
 // 初始化Peer结构体
 int initialize_peer(Peer *peer)
 {
-    if(peer=NULL)    return -1;
-    peer->socket=-1;
+    if (peer == NULL)    return -1;
+    peer->socket = -1;
     memset(peer->ip,0,16);
-    peer->port=0;
+    peer->port = 0;
     memset(peer->id,0,21);
-    peer->state=INITIAL;
+    peer->state = INITIAL;
 
-    peer->in_buff=NULL;
-    peer->out_msg=NULL;
-    peer->out_msg_copy=NULL;
+    peer->in_buff = NULL;
+    peer->out_msg = NULL;
+    peer->out_msg_copy = NULL;
 
-    peer->in_buff=(char*)malloc(MSG_SIZE);
-    if(peer->in_buff==NULL)    goto OUT;
+    peer->in_buff = (char*)malloc(MSG_SIZE);
+    if (peer->in_buff == NULL)    goto OUT;
     memset(peer->in_buff,0,MSG_SIZE);
-    peer->buff_len=0;
+    peer->buff_len = 0;
 
-    peer->out_msg=(char*)malloc(MSG_SIZE);
-    if(peer->out_msg==NULL)    goto OUT;
+    peer->out_msg = (char*)malloc(MSG_SIZE);
+    if (peer->out_msg == NULL)    goto OUT;
     memset(peer->out_msg,0,MSG_SIZE);
-    peer->msg_len=0;
+    peer->msg_len = 0;
 
-    peer->out_msg_copy=(char*)malloc(MSG_SIZE);
-    if(peer->out_msg_copy==NULL)    goto OUT;
+    peer->out_msg_copy = (char*)malloc(MSG_SIZE);
+    if (peer->out_msg_copy == NULL)    goto OUT;
     memset(peer->out_msg_copy,0,MSG_SIZE);
-    peer->msg_copy_len=0;
-    peer->msg_copy_index=0;
+    peer->msg_copy_len = 0;
+    peer->msg_copy_index = 0;
 
-    peer->am_choking=1;
-    peer->am_interested=0;
-    peer->peer_choking=1;
-    peer->peer_interested=0;
+    peer->am_choking = 1;
+    peer->am_interested = 0;
+    peer->peer_choking = 1;
+    peer->peer_interested = 0;
 
-    peer->bitmap.bitfield=NULL;
-    peer->bitmap.bitfield_length=0;
-    peer->bitmap.valid_length=0;
+    peer->bitmap.bitfield = NULL;
+    peer->bitmap.bitfield_length = 0;
+    peer->bitmap.valid_length = 0;
 
-    peer->Request_piece_head=NULL;
-    peer->Requested_piece_head=NULL;
+    peer->Request_piece_head = NULL;
+    peer->Requested_piece_head = NULL;
 
-    peer->down_total=0;
-    peer->up_total=0;
+    peer->down_total = 0;
+    peer->up_total = 0;
 
-    peer->start_timestamp=0;
-    peer->recet_timestamp=0;
+    peer->start_timestamp = 0;
+    peer->recet_timestamp = 0;
 
-    peer->last_down_timestamp=0;
-    peer->last_up_timestamp=0;
-    peer->down_count=0;
-    peer->up_count=0;
-    peer->down_rate=0.0;
-    peer->up_rate=0.0;
+    peer->last_down_timestamp = 0;
+    peer->last_up_timestamp = 0;
+    peer->down_count = 0;
+    peer->up_count = 0;
+    peer->down_rate = 0.0;
+    peer->up_rate = 0.0;
     
-    peer->next=(Peer*)0;
+    peer->next = (Peer*)0;
     return 0;
 OUT:
-    if(peer->in_buff!=NULL)             free(peer->in_buff);
-    if(peer->out_msg!=NULL)             free(peer->out_msg);
-    if(peer->out_msg_copy!=NULL)        free(peer->out_msg_copy);
+    if (peer->in_buff != NULL)             free(peer->in_buff);
+    if (peer->out_msg != NULL)             free(peer->out_msg);
+    if (peer->out_msg_copy != NULL)        free(peer->out_msg_copy);
     return -1;
 };
 
@@ -88,24 +88,24 @@ Peer *add_peer_node()
     Peer *node,*p;
 
     //分配内存空间
-    node=(Peer*)malloc(sizeof(Peer));
-    if(node==NULL){
+    node = (Peer*)malloc(sizeof(Peer));
+    if (node == NULL) {
         printf("%s:%d error\n",__FILE__,__LINE__);
         return NULL;
     }
     //进行初始化
-    ret=initialize_peer(node);
-    if(ret<0){
+    ret = initialize_peer(node);
+    if (ret < 0) {
         printf("%s:%d error\n",__FILE__,__LINE__);
         free(node);
         return NULL;
     }
     //将node加入到peer链表中
-    if(peer_head==NULL)    {peer_head=;}    //node为peer链表的第一个结点
+    if (peer_head == NULL)    {peer_head = node;}    //node为peer链表的第一个结点
     else{
-        p=peer_head;    //使p指针指向peer链表的最后一个结点
-        while(p->next!=NULL)    p=p->next;
-        p->next=node;
+        p = peer_head;    //使p指针指向peer链表的最后一个结点
+        while (p->next != NULL)    p=p->next;
+        p->next = node;
     }
     return node;
 };
@@ -113,18 +113,18 @@ Peer *add_peer_node()
 // 从peer链表中删除一个结点
 int del_peer_node(Peer node)
 {
-    Peer *p=peer_head,*q;
-    if(peer==NULL)    return -1;
+    Peer *p = peer_head,*q;
+    if(peer == NULL)    return -1;
 
-    while(p!=NULL){
-        if(p==peer){
-            if(p==peer_head)    peer_head=p->next;
-            else    q->next=p->next;
+    while(p != NULL){
+        if(p == peer) {
+            if(p == peer_head)    peer_head = p->next;
+            else    q->next = p->next;
             free_peer_node(p);
             return 0;
-        }else{
-            q=p;
-            p=p->next;
+        }else {
+            q = p;
+            p = p->next;
         }
     }
 
@@ -136,11 +136,11 @@ int cancel_request_list(Peer *node)
 {
     Request_piece *p=node->Request_piece_head;
 
-    while(p!=NULL)
+    while(p != NULL)
     {
-        node->Request_piece_head=node->Request_piece_head->next;
+        node->Request_piece_head = node->Request_piece_head->next;
         free(p);
-        p=node->Request_piece_head;
+        p = node->Request_piece_head;
     }
 
     return 0;
@@ -149,12 +149,12 @@ int cancel_request_list(Peer *node)
 // 撤销当前被请求队列
 int cancel_requested_list(Peer *node)
 {
-    Request_piece *p=node->Request_piece_head;
+    Request_piece *p = node->Request_piece_head;
 
-    while(p!=NULL){
-        node->Requested_piece_head=node->Requested_piece_head->next;
+    while (p != NULL) {
+        node->Requested_piece_head = node->Requested_piece_head->next;
         free(p);
-        p=node->Requested_piece_head;
+        p = node->Requested_piece_head;
     }
 
     return 0;
@@ -162,9 +162,29 @@ int cancel_requested_list(Peer *node)
 
 // 释放一个peer结点的内存
 void free_peer_node(Peer *node);
+{
+    if (node == NULL) {
+        printf("node does not exit!\n");
+    } else {
+        Peer *p;
+        p = node->next;
+        free(node);
+        node = p;
+    }
+};
 
 // 释放peer管理模块中动态申请的内存
 void release_memory_in_peer();
+{
+    Peer *p = peer_head->next;
+    if (p == NULL) {
+        printf("peer is already null\n");
+    } else {
+        while (p != NULL) {
+            free_peer_node(p);
+        }
+    }
+};
 
 // 打印peer结点的一些信息，用于调试程序
 void print_peers_data();
